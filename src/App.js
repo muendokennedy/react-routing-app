f0deae9d4b3e9dcd0e9d46363d7de78b27fed05d
muendokennedy <kennedymunyao999@gmail.com>
2024-02-21 14:01:30 +0300

    Added all the other components which represent the other pages and implemented react router dom dependency successfully

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Home from './Home';
+import PostPage from './PostPage';
+import Missing from './Missing';
+import NewPost from './NewPost';
+import Footer from './Footer';
+import About from './About';
+import { BrowserRouter as Router, Routes, Route,useHistory} from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Routes>
+            <Route exact path='/' element={
+            <>
+            <Header/>
+            <Nav/>
+            <Home/>
+            <Footer/>
+            </>
+            }/>
+            <Route exact path='/post' element={
+              <>
+              <Header/>
+              <Nav/>
+              <NewPost/>
+              <Footer/>
+              </>
+            }/>
+            <Route exact path='/post/:id' element={
+              <>
+              <Header/>
+              <Nav/>
+              <PostPage/>
+              <Footer/>
+              </>
+            }/>
+            <Route exact path='/about' element={
+              <>
+              <Header/>
+              <Nav/>
+              <About/>
+              <Footer/>
+              </>
+            }/>
+            <Route exact path='*' element={
+              <>
+              <Header/>
+              <Nav/>
+              <Missing/>
+              <Footer/>
+              </>
+            }/>
+          </Routes>
+        </Router>
     </div>
   );
 }
