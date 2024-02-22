1201430bbe5691e07268c4b0bcef68bc8a8b806f
muendokennedy <kennedymunyao999@gmail.com>
2024-02-22 14:58:16 +0300

    Modified the app component to define the search function and also modified the appearance of the about, the footer and the missing page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Footer from './Footer';
 import About from './About';
 import { Route, useHistory, Switch} from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 
 
 
@@ -47,7 +48,29 @@ function App() {
   ]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
-  const history = useHistory()
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    const filteredResults = posts.filter(post => ((post.body).toLowerCase()).includes(search.toLowerCase())
+    || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format( new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody};
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    history.push('/');
+  }
 
   const handleDelete = (id) => {
 
@@ -62,10 +85,16 @@ function App() {
        <Nav search={search} setSearch={setSearch}/>
        <Switch>
             <Route exact path='/'>
-                <Home posts={posts}/>
+                <Home posts={searchResults}/>
             </Route>
             <Route exact path='/post'>
-                <NewPost/>
+                <NewPost
+                  handleSubmit={handleSubmit}
+                  postTitle={postTitle}
+                  setPostTitle={setPostTitle}
+                  postBody={postBody}
+                  setPostBody={setPostBody}
+                />
             </Route>
             <Route exact path='/post/:id'>
                 <PostPage posts={posts} handleDelete={handleDelete}/>
--- src/NewPost.jsx
+++ src/NewPost.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const NewPost = () => {
+const NewPost = ({ handleSubmit, postTitle, setPostTitle, postBody, setPostBody }) => {
   return (
-    <main>
-      <h1>NewPost</h1>
+    <main className='NewPost'>
+      <h2>NewPost</h2>
+      <form className='newPostForm' onSubmit={handleSubmit}>
+        <label htmlFor='postTitle'>Title: </label>
+        <input type='text' id='postTitle' value={postTitle} required onChange={(e) => setPostTitle(e.target.value)}/>
+        <label htmlFor='postBody'>Body: </label>
+        <textarea id='postBody' required onChange={(e) => setPostBody(e.target.value)}>{postBody}</textarea>
+        <button type='submit'>Publish Post</button>
+      </form>
     </main>
   )
 }
