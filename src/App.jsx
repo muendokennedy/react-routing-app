46e5b5be388fea7623bf16eaa580308eaaa0c7c4
muendokennedy <kennedymunyao999@gmail.com>
2024-02-22 13:29:09 +0300

    Added the Feed and the Post components to display the posts in the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,65 @@ import { Route, useHistory, Switch} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 
+
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'My First Post',
+      datetime: 'July 01 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, repellendus. Atque neque iste laudantium tenetur!'
+    },
+    {
+      id: 2,
+      title: 'My Second Post',
+      datetime: 'July 01 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, repellendus. Atque neque iste laudantium tenetur!'
+    },
+    {
+      id: 3,
+      title: 'My Third Post',
+      datetime: 'July 01 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, repellendus. Atque neque iste laudantium tenetur!'
+    },
+    {
+      id: 4,
+      title: 'My Fourth Post',
+      datetime: 'July 01 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, repellendus. Atque neque iste laudantium tenetur!'
+    },
+    {
+      id: 5,
+      title: 'My Fifth Post',
+      datetime: 'July 01 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, repellendus. Atque neque iste laudantium tenetur!'
+    }
+  ]);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const history = useHistory()
+
+  const handleDelete = (id) => {
+
+    const postList = posts.filter(post => post.id !== id)
+    setPosts(postList);
+    history.push('/');
+  }
+
   return (
     <div className="App">
-       <Header/>
-       <Nav/>
+       <Header title='React JS Blog'/>
+       <Nav search={search} setSearch={setSearch}/>
        <Switch>
             <Route exact path='/'>
-                <Home/>
+                <Home posts={posts}/>
             </Route>
             <Route exact path='/post'>
                 <NewPost/>
             </Route>
             <Route exact path='/post/:id'>
-                <PostPage/>
+                <PostPage posts={posts} handleDelete={handleDelete}/>
             </Route>
             <Route exact path='/about' component={About}/>
             <Route exact path='*' component={Missing}/>
