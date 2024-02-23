127024f571bfb7e01e254b9dfbd887216d9758ff
muendokennedy <kennedymunyao999@gmail.com>
2024-02-23 14:07:22 +0300

    Made a local development json server and created the edit post page to edit the posts already published in the app

--- src/App.jsx
+++ src/App.jsx
@@ -9,48 +9,43 @@ import About from './About';
 import { Route, useHistory, Switch} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
-
+import api from './api/Post';
+import EditPost from './EditPost';
 
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: 'My First Post',
-      datetime: 'July 01 2021 11:17:36 AM',
-      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, repellendus. Atque neque iste laudantium tenetur!'
-    },
-    {
-      id: 2,
-      title: 'My Second Post',
-      datetime: 'July 01 2021 11:17:36 AM',
-      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, repellendus. Atque neque iste laudantium tenetur!'
-    },
-    {
-      id: 3,
-      title: 'My Third Post',
-      datetime: 'July 01 2021 11:17:36 AM',
-      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, repellendus. Atque neque iste laudantium tenetur!'
-    },
-    {
-      id: 4,
-      title: 'My Fourth Post',
-      datetime: 'July 01 2021 11:17:36 AM',
-      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, repellendus. Atque neque iste laudantium tenetur!'
-    },
-    {
-      id: 5,
-      title: 'My Fifth Post',
-      datetime: 'July 01 2021 11:17:36 AM',
-      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, repellendus. Atque neque iste laudantium tenetur!'
-    }
-  ]);
+  const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
   const history = useHistory();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+
+        const response = await api.get('/posts');
+
+        setPosts(response.data)
+        
+      } catch (error) {
+        if(error.response){
+          // Not in the 200 response range
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else {
+          console.log(`Error: ${error.message}`);
+        }
+      }
+    }
+
+    fetchPosts();
+  }, [])
 
   useEffect(() => {
     const filteredResults = posts.filter(post => ((post.body).toLowerCase()).includes(search.toLowerCase())
@@ -65,19 +60,52 @@ function App() {
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format( new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title: postTitle, datetime, body: postBody};
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    history.push('/');
+
+    const createPosts = async () => {
+      
+      try {
+        const response = await api.post('/posts', newPost);
+        const allPosts = [...posts, response.data];
+        setPosts(allPosts);
+        setPostTitle('');
+        setPostBody('');
+        history.push('/');
+      } catch (error) {
+        console.log(`Error: ${error.message}`);  
+      }
+    }
+
+    createPosts();
+  }
+  
+  const handleEdit = async (id) => {
+    const datetime = format( new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = { id, title: editTitle, datetime, body: editBody};
+
+    try {
+
+      const response = await api.put(`/posts/${id}`, updatedPost);
+
+      setPosts(posts.map(post => post.id === id ? {...response.data } : post));
+      setEditTitle('');
+      setEditBody('');
+      history.push('/');
+    } catch (error) {
+      console.log(`Error: ${error.message}`);  
+    }
+  }
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postList = posts.filter(post => post.id !== id)
+      setPosts(postList);
+      history.push('/');
+    } catch (error) {
+      console.log(`Error: ${error.message}`);  
+    }
+
   }
 
-  const handleDelete = (id) => {
-
-    const postList = posts.filter(post => post.id !== id)
-    setPosts(postList);
-    history.push('/');
-  }
 
   return (
     <div className="App">
@@ -94,6 +122,16 @@ function App() {
                   setPostTitle={setPostTitle}
                   postBody={postBody}
                   setPostBody={setPostBody}
+                />
+            </Route>
+            <Route path='/edit/:id'>
+                <EditPost
+                posts={posts}
+                  handleEdit={handleEdit}
+                  editTitle={editTitle}
+                  setEditTitle={setEditTitle}
+                  editBody={editBody}
+                  setEditBody={setEditBody}
                 />
             </Route>
             <Route exact path='/post/:id'>
---
+++ src/EditPost.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+const EditPost = ({ posts, handleEdit, editTitle, setEditTitle, editBody, setEditBody}) => {
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() === id);
+
+  useEffect(() => {
+    if(post){
+      setEditTitle(post.title);
+      setEditBody(post.body);
+    }
+  }, [post, setEditTitle, setEditBody])
+  return (
+    <main className='NewPost'>
+      {editTitle && 
+      <>
+        <h2>Editpost</h2>
+        <form className='newPostForm' onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor='postTitle'>Title: </label>
+          <input type='text' id='postTitle' value={editTitle} required onChange={(e) => setEditTitle(e.target.value)}/>
+          <label htmlFor='postBody'>Body: </label>
+          <textarea id='postBody' required onChange={(e) => setEditBody(e.target.value)}>{editBody}</textarea>
+          <button type='submit' onClick={() => handleEdit(post.id)}>Edit Post</button>
+        </form>
+      </>
+      }
+  </main>
+  )
+}
+
+export default EditPost
